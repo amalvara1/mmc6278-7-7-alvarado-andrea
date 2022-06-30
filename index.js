f8c41028b760589e519a8fb0dbad13af24e6965b
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-29 22:37:34 -0400

    added questionArr 1-3

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+var questionArr = [
+    {
+        question: 'What famous painter is known for their vibrant self portraits?',
+        answer: ' Frida Kahlo',
+        options: [
+            'Pablo Picasso',
+            'Salvador Dali',
+            'Frida Kahlo',
+            'Andy Warhol',
+        ]
+    },
+    {
+        question: 'Who sang Happy Birthday Mr.President to John F. Kennedy in 1962?',
+        answer: 'Marilyn Monroe',
+        options: [
+            'Audrey Hepburn',
+            'Marilyn Monroe',
+            'Judy Garland',
+            'Jackie kennedy',
+        ]
+    },
+    {
+        question: 'What two siblings are famous for their games on Nintendo',
+        answer: 'Mario and Luigi',
+        options: [
+            'Dexter and DeeDee',
+            'Mario and Luigi',
+            'Tommy and Chuckie',
+            'Cat and dog',
+        ]
+    },
+    {
+        question: '',
+        answer: '',
+        options: [
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        question: '',
+        answer: '',
+        options: [
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+]
