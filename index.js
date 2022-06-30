43370edad6b5cf557e18b65e3dee2295d745cdf7
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-06-30 15:21:44 -0400

    added functions and var to run through questions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var questionArr = [
     {
         question: 'What famous painter is known for their vibrant self portraits?',
-        answer: ' Frida Kahlo',
+        answer: 'Frida Kahlo',
         options: [
             'Pablo Picasso',
             'Salvador Dali',
@@ -53,24 +53,68 @@ var questionArr = [
 ]
 
 
-var startquiz = true;
+//var startquiz = true;
 
 var quiz = document.getElementById("quiz");
+var questionIndex = 0;
+var correctAnswers = 0;
+
+
+function gradeQuiz(){
+    console.log(correctAnswers)
+    var percentage = correctAnswers/questionArr.length
+    console.log(percentage)
+    quiz.innerHTML = ""
+    quiz.innerHTML = percentage
+}
 
 //When page loads - create "start quiz" button element within the div 'quiz'
 function startQuiz(){
     var btn = document.createElement("button");
-    button.innerHTML = "Start Quiz!";
+    btn.innerHTML = "Start Quiz!";
     btn.id = "start-quiz";
     btn.type = "submit";
-    document.quiz.appendChild(btn);
+    quiz.appendChild(btn);
     
-    btn.addEventListener("click", function(){
+    btn.addEventListener("click", function(e){
+        //console.log('clicked')
+        showQuestion()
 
     })
 
 }
 
+startQuiz()
+
+
+function showQuestion(){
+    quiz.innerHTML = ""
+    var question = questionArr[questionIndex]
+    //console.log(question)
+    var p = document.createElement("p");
+    p.innerHTML = question.question
+    quiz.appendChild(p);
+
+    var div = document.createElement("div");
+    for (let index = 0; index < question.options.length; index++) {
+        const element = question.options[index];
+        //console.log(element)
+        var btn = document.createElement("button")
+        btn.innerHTML = element
+        div.appendChild(btn)
+        btn.addEventListener("click", function(e){
+            const selection = (e.target.innerHTML)
+            if( selection === question.answer){
+                correctAnswers++
+            } 
+                questionIndex++
+                if(questionIndex === questionArr.length){
+                    gradeQuiz()
+                } else{showQuestion()}
+        })
+    }
+        quiz.appendChild(div)
+}
 //setInterval function()
 
 //clearInterval function()
